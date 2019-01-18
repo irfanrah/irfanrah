047f987e597c94325418b81c2d151010f487b9f0
irfanrah <rahadikirfan@gmail.com>
2019-01-18 16:46:40 +0700

    Add files via upload

---
+++ coba.cpp
@@ -0,0 +1,38 @@
+#include "opencv2/highgui/highgui.hpp"
+#include <iostream>
+#include <opencv2/imgproc/imgproc.hpp>
+
+
+using namespace cv;
+using namespace std;
+Mat Image,Gray,GaussianB,edge,draw,ROI,Image2;
+
+
+
+
+int main(int argc, char* argv[])
+{
+	Image = imread("lena.jpg");
+	cvtColor(Image,Gray,COLOR_BGR2GRAY);
+	GaussianBlur(Gray, GaussianB, Size(5,5), 0, 0);
+	// Canny Edge
+	Canny( GaussianB, edge, 50, 150, 3);
+	edge.convertTo(draw, CV_8U);
+	// Select ROI
+	Mat roiMuka = Gray( Rect(200,200,200,200) ); // rect(x,y,width,height)
+	roiMuka.setTo(Scalar::all(0)); // fill 
+	Gray.copyTo(Image2) ; // Duplicate
+	
+	Rect WhereRec(200, 200, roiMuka.cols, roiMuka.rows); // Posisi dan ukuran
+	roiMuka.copyTo(Gray(WhereRec)); // copy
+	
+	//Bitwise
+    bitwise_xor(draw,Gray,ROI); // Region Of Interest
+    
+    // Muncul
+	namedWindow("hasil",CV_WINDOW_AUTOSIZE);
+	imshow("hasil",ROI);
+	waitKey(0);
+	
+}
+
